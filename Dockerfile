f94e3e63d71877374748dc3585db514a53617574
Sebastiaan van Stijn <github@gone.nl>
2017-11-23 16:23:04 +0100

    Add more runtime customisation options
    Add options to set the page-title, and spec-url through
    environment variables
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM nginx:1.13-alpine
+ENV PAGE_TITLE=ReDoc
+ENV SPEC_URL=swagger.yaml
 ENV REDOC_OPTIONS=
 COPY run.sh /run.sh
 CMD ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e
+sed -i -e "s/\$PAGE_TITLE/$PAGE_TITLE/g" /usr/share/nginx/html/index.html
+sed -i -e "s/\$SPEC_URL/$SPEC_URL/g" /usr/share/nginx/html/index.html
 sed -i -e "s/\$REDOC_OPTIONS/$REDOC_OPTIONS/g" /usr/share/nginx/html/index.html
 exec nginx -g "daemon off;"
